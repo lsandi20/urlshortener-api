76d5f4df5c061d58a3a5cb793ff9cd145d7f4514
zand <luthfisandi20@gmail.com>
2022-08-13 15:09:03 +0700

    read links from database

--- services/services.go
+++ services/services.go
@@ -1,20 +1,40 @@
 package services
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/lsandi20/urlshortener-api/models"
+	"github.com/lsandi20/urlshortener-api/database"
+	"github.com/teris-io/shortid"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-var links = []models.Link{
-	{ID: "1", Original: "https://github.com/lsandi20", Short: "http://localhost:8080/lsandi", Clicks: 0},
-}
+var short, err = shortid.Generate()
 
 func RedirectURL(c *gin.Context) {
 	c.Redirect(http.StatusMovedPermanently, "https://github.com/lsandi20")
 }
 
 func GetLinks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, links)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	coll := database.Db.Collection("links")
+
+	cursor, err := coll.Find(ctx, bson.M{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	var urlShorternerResult []bson.M
+
+	if err = cursor.All(ctx, &urlShorternerResult); err != nil {
+		panic(err)
+	}
+
+	c.JSON(http.StatusOK, urlShorternerResult)
 }
