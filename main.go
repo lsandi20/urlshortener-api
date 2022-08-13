055c870655d92afef9b232a13ba75454c41bb959
zand <luthfisandi20@gmail.com>
2022-08-13 15:08:51 +0700

    integrate with database

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"context"
+	"time"
+
+	"github.com/lsandi20/urlshortener-api/config"
 	"github.com/lsandi20/urlshortener-api/controllers"
+	"github.com/lsandi20/urlshortener-api/database"
 )
 
 func main() {
+	config.ReadConfig()
+
+	database.InitDatabase()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	defer func() {
+		if err := database.DbClient.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
 	router := controllers.AssignRoutes()
 	router.Run("localhost:8080")
 }
