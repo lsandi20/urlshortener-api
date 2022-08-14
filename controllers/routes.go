4bd708ce5107ebd443a30c31d1be353d19a2568d
zand <luthfisandi20@gmail.com>
2022-08-15 00:37:43 +0700

    add cors support and update post link

--- controllers/routes.go
+++ controllers/routes.go
@@ -1,14 +1,26 @@
 package controllers
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/lsandi20/urlshortener-api/services"
 )
 
 func AssignRoutes() (router *gin.Engine) {
 	router = gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "OPTIONS", "DELETE"}
+	config.AllowHeaders = []string{"Origin", "access-control-allow-origin", "acess-control-allow-headers", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"}
+	config.AllowCredentials = true
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.MaxAge = 12 * time.Hour
+	router.Use(cors.New(config))
+
 	router.GET("/links", services.GetLinks)
-	router.POST("/links", services.CreateLink)
+	router.POST("/link", services.CreateLink)
 	router.GET("/:short", services.RedirectURL)
 	return
 }
